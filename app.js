561b5536b5f8fc3fd19cceda21eac2f8e6d34d25
Jerrylee <jerrylee.davis@hotmail.co.uk>
2024-03-27 16:46:56 +0000

    Planet Collector

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Hello World')
+// console.log('Hello World');
+// prompt("Do you like space?");
+// if("yes")alert("Great, then you've come to the right place");
+// else("Oh well, hope to see you again!");
+
+const button = document.querySelector("button");
+button.addEventListener("click", function(){
+    console.log("button click")
+})
