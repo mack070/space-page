58bb7d58f00eed56d4de21da86b4e7ac6be1ec43
Jerrylee <jerrylee.davis@hotmail.co.uk>
2024-03-31 23:02:13 +0100

    update-4

--- app.js
+++ app.js
@@ -1,12 +1,61 @@
-const moonButton = document.getElementById("MoonButton");
-const planetButton = document.getElementById("planetButton")
-const sunButton = document.getElementById("sunButton")
-const galaxyButton = document.getElementById("galaxyButton")
-const upgradeButton = document.getElementById("upgradeButton")
-const planetsSpan = document.getElementById("planetsSpan")
-const ppsSpan = document.getElementById("ppsSpan")
+const planetButton = document.getElementById("planetButton");
+const moonButton = document.getElementById("moonButton");
+const sunButton = document.getElementById("sunButton");
+const galaxyButton = document.getElementById("galaxyButton");
+const upgradeButton = document.getElementById("upgradeButton");
+const planetsSpan = document.getElementById("planetsSpan");
+const ppsSpan = document.getElementById("ppsSpan");
+
+
+planetButton.addEventListener("click", savePlanet);
+moonButton.addEventListener("click", moonUpgrade);
+sunButton.addEventListener("click", sunUpgrade);
+galaxyButton.addEventListener("click", galaxyUpgrade);
+
+
+function updatePage() {
+planetsSpan.textContent = stats.planetsCount;
+ppsSpan.textContent = stats.pps;
+}
 
 const stats = {
-planetsCount: 0;
-stats: 0;
+planetsCount: 0,
+pps: 0, // Planets Per Second
+};
+
+function savePlanet() {
+stats.planetsCount++;
+updatePage();
+updateStorage();
+}
+
+function moonUpgrade() {
+stats.pps++;
+stats.planetsCount -= stats.pps * 10;
+updatePage();
+updateStorage();
+}
+
+function sunUpgrade() {
+stats.pps++;
+stats.planetsCount -= stats.pps * 50;
+updatePage();
+updateStorage();
+}
+
+function galaxyUpgrade() {
+stats.pps++;
+stats.planetsCount -= stats.pps * 100;
+updatePage();
+updateStorage();
+}
+
+setInterval(function(){
+stats.planetsCount += stats.pps;
+updatePage();
+updateStorage();
+}, 1000);
+
+function updateStorage( ){
+localStorage.setItem("stats", JSON.stringify(stats))
 }
